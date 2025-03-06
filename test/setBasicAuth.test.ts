ef447cf9acb1f04623875e59f43131676c248d60
Raul Raat <raul.raat@delfi.ee>
2025-03-06 22:31:53 +0200

    Add test for setBasicAuth

--- test/setBasicAuth.test.ts
+++ test/setBasicAuth.test.ts
@@ -1,8 +1,17 @@
 import { describe, expect, test } from '@jest/globals';
-import { PROXY_API_HOST, PROXY_API_PORT, proxyRequest, TEST_SERVER_HOST_FOR_PROXY } from './global.server';
+import { INVALID_PORT_FOR_TEST, PROXY_API_HOST, PROXY_API_PORT, proxyRequest, TEST_SERVER_HOST_FOR_PROXY } from './global.server';
 import BrowserMobProxyAPIClient from '../index';
+import BMPError from '../BMPError';
 
 describe('setBasicAuth', () => {
+  test('should throw error for invalid port', async () => {
+    const API = new BrowserMobProxyAPIClient(PROXY_API_HOST, PROXY_API_PORT);
+
+    await expect(API.setBasicAuth(INVALID_PORT_FOR_TEST, TEST_SERVER_HOST_FOR_PROXY, 'user', 'test'))
+      .rejects
+      .toThrow(BMPError);
+  });
+
   test('setBasicAuth', async () => {
     const API = new BrowserMobProxyAPIClient(PROXY_API_HOST, PROXY_API_PORT);
     const port = await API.start();
